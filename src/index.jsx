b99cb759521f51c91f0451c60dc058d986537424
Dali Duraku <mesherbetmolla@gmail.com>
2025-02-20 14:49:59 -0500

    feat: restructuring the folders and files

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from "./pages/Vans"
+import Vans from "./pages/Vans/Vans"
 import Layout from './components/Layout'
 
 import "./server"
