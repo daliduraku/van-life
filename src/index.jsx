11f0b6e19228f055ef9d5b87c6dd539d742bc7f9
Dali Duraku <mesherbetmolla@gmail.com>
2025-02-22 18:16:46 -0500

    feat: have the user go to login page
    - if the user is not authorized we have them navigated to the login page

--- src/index.jsx
+++ src/index.jsx
@@ -17,6 +17,7 @@ import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 import NotFound from './pages/NotFound';
+import AuthRequired from './components/AuthRequired'
 
 import "./server"
 
@@ -34,17 +35,21 @@ function App() {
             element={<Login />}
           />
           
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />}>
-              <Route index element={<HostVanInfo />} />
-              <Route path="pricing" element={<HostVanPricing />} />
-              <Route path="photos" element={<HostVanPhotos />} />
+
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
             </Route>
           </Route>
+
           <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
