6a37fe9d77c8ab55ce907f62ce8c4a03a2f2b203
Dali Duraku <mesherbetmolla@gmail.com>
2025-02-22 15:04:20 -0500

    feat: create a login page
    - update the routes and and header component

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/Vans/VanDetail"
+import Login from "./pages/Login"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
@@ -28,6 +29,10 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
+          <Route
+            path="login"
+            element={<Login />}
+          />
           
           <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
