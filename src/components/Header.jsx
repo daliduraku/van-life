b11f8943d2cd08b5d222a5700ae3962b4d231a08
Dali Duraku <mesherbetmolla@gmail.com>
2025-02-23 14:50:00 -0500

    feat: add a fake logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, NavLink } from "react-router-dom"
-import imageUrl from "/src/assets/images/avatar-icon.png"
+import imageUrl from "/assets/images/avatar-icon.png"
 
 export default function Header() {
     const activeStyles = {
@@ -8,35 +8,40 @@ export default function Header() {
         textDecoration: "underline",
         color: "#161616"
     }
-    
+
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <NavLink 
-                    to="host"
-                    style={({isActive}) => isActive ? activeStyles : null}
+                <NavLink
+                    to="/host"
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
-                <NavLink 
-                    to="about"
-                    style={({isActive}) => isActive ? activeStyles : null}
+                <NavLink
+                    to="/about"
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
-                <NavLink 
-                    to="vans"
-                    style={({isActive}) => isActive ? activeStyles : null}
+                <NavLink
+                    to="/vans"
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
                 <Link to="login" className="login-link">
-                    <img 
+                    <img
                         src={imageUrl}
                         className="login-icon"
                     />
                 </Link>
+                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
